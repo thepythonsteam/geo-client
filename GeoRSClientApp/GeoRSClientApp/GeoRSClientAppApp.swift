f6df15fcdff0744f15fd2deb848a03f57268a8b0
Alexander Litvintsev <alexanderlitvintsev@mail.ru>
2022-01-08 18:50:11 +0300

    init ios app

---
+++ GeoRSClientApp/GeoRSClientApp/GeoRSClientAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoRSClientAppApp.swift
+//  GeoRSClientApp
+//
+//  Created by Alexander Litvintsev on 08.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct GeoRSClientAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
